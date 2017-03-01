178ebd16340eca9d8bb75c81be9dece713efc97f
kent930 <quentingrail8@gmail.com>
2017-03-01 16:14:19 +0100

    first commit

---
+++ src/main/java/ensai/Send.java
@@ -0,0 +1,35 @@
+package ensai;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+
+
+public class Send {
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv)
+			throws java.io.IOException, TimeoutException {
+		
+
+		// create a connection to the server
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		//declare a queue for us to send to; then we can publish a message to the queue
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    String message = "Hello !";
+	    channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+	    System.out.println(" [x] Sent '" + message + "'");
+	    
+	    //Lastly, we close the channel and the connection
+	    channel.close();
+	    connection.close();
+	}
+}
+
