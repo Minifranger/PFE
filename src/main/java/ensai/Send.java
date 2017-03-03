496db043cf6fb084385d6dbfaa44ef7d225951b7
kent930 <quentingrail8@gmail.com>
2017-03-03 15:25:19 +0100

    Gestion du temps d'attente pour l'envoi de données
    Tuples correctement formatés dans Flink

--- src/main/java/ensai/Send.java
+++ src/main/java/ensai/Send.java
@@ -17,21 +17,34 @@ public class Send {
 
 	public static void main(String[] argv) throws java.io.IOException, TimeoutException {
 
+		int waitTime = 10;
+		int i = 1;
 		// create a connection to the server
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
+//		File f = new File("./ressources/molding_machine_10M.nt");
 		File f = new File("./ressources/molding_machine_10M.nt");
+
 		BufferedReader br = new BufferedReader(new FileReader(f));
 		String line;
 		// declare a queue for us to send to; then we can publish a message to
 		// the queue
 		channel.queueDeclare(QUEUE_NAME, true, false, false, null);
 		while ((line = br.readLine()) != null) {
+			
+			if (i%945 == 0){
+				try{
+					System.out.println("Waaaaaaaaaaaaait");
+					Thread.sleep(waitTime*1000);
+				}catch (InterruptedException e) {}
+			}
+			
 			channel.basicPublish("", QUEUE_NAME, null, line.getBytes());
 			System.out.println(" [x] Sent '" + line + "'");
+			i++;
 		}
 		br.close();
 
--- src/main/java/ensai/StreamRabbitMQ.java
+++ src/main/java/ensai/StreamRabbitMQ.java
@@ -1,5 +1,9 @@
 package ensai;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -19,16 +23,16 @@ package ensai;
  */
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.rabbitmq.RMQSource;
 import org.apache.flink.streaming.connectors.rabbitmq.common.RMQConnectionConfig;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.util.Collector;
-
-import ensai.SocketTextStreamWordCount.LineSplitter;
 
 /**
  * This example shows an implementation of WordCount with data from a text
@@ -42,8 +46,7 @@ import ensai.SocketTextStreamWordCount.LineSplitter;
  * 
  * 
  * <p>
- * Usage:
- * <code>SocketTextStreamWordCount &lt;hostname&gt; &lt;port&gt;</code>
+ * Usage: <code>SocketTextStreamWordCount &lt;hostname&gt; &lt;port&gt;</code>
  * <br>
  * 
  * <p>
@@ -58,74 +61,163 @@ import ensai.SocketTextStreamWordCount.LineSplitter;
  */
 public class StreamRabbitMQ {
 	//
-	//	Program
+	// Program
 	//
+
+	public static int currentObsGroup;
+	public static Map<Integer, Integer> mapObsSensors = new HashMap<Integer, Integer>();
+	public static String currentTimestamp;
+	public static int currentMachine;
 
 	public static void main(String[] args) throws Exception {
 
-	
+		List<Integer> listSensors = new ArrayList<Integer>();
+		listSensors.add(3);
+		listSensors.add(17);
+		listSensors.add(19);
+		listSensors.add(20);
+		listSensors.add(23);
+
+
+		String currentTimestamp = null;
+		int currentMachine = 0;
+
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+		final RMQConnectionConfig connectionConfig = new RMQConnectionConfig.Builder().setHost("localhost")
+				.setPort(5672).setUserName("guest").setPassword("guest").setVirtualHost("/").build();
 
-		final RMQConnectionConfig connectionConfig = new RMQConnectionConfig.Builder()
-		    .setHost("localhost")	
-		    .setPort(5672)
-		    .setUserName("guest")
-		    .setPassword("guest")
-		    .setVirtualHost("/")
-		    .build();
-		    
 		final DataStream<String> stream = env
-		    .addSource(new RMQSource<String>(
-		        connectionConfig,            // config for the RabbitMQ connection
-		        "coucou",                 // name of the RabbitMQ queue to consume
-		        true,                        // use correlation ids; can be false if only at-least-once is required
-		        new SimpleStringSchema()))   // deserialization schema to turn messages into Java objects
-		    .setParallelism(1);  
+				.addSource(new RMQSource<String>(connectionConfig, // config for
+																	// the
+																	// RabbitMQ
+																	// connection
+						"coucou", // name of the RabbitMQ queue to consume
+						true, // use correlation ids; can be false if only
+								// at-least-once is required
+						new SimpleStringSchema())) // deserialization schema to
+													// turn messages into Java
+													// objects
+				.setParallelism(1);
 
-		DataStream<Tuple3<String, String, String>> res  = 	stream.flatMap(new LineSplitter());
+		// stream.print();
 
-		res.print();
+		DataStream<Tuple3<String, String, String>> res = stream.map(new LineSplitter());
+
+		//res.print();
+
+		DataStream<Tuple4<Integer, Integer, Float, String>> st = res
+				.flatMap(new InputAnalyze(listSensors));
+
+		 st.print();
 
 		// execute program
 		env.execute("Java WordCount from SocketTextStream Example");
 	}
 
 	//
-	// 	User Functions
+	// User Functions
 	//
 
 	/**
-	 * Implements the string tokenizer that splits sentences into words as a user-defined
-	 * FlatMapFunction. The function takes a line (String) and splits it into
-	 * multiple pairs in the form of "(word,1)" (Tuple2<String, Integer>).
+	 * Implements the string tokenizer that splits sentences into words as a
+	 * user-defined FlatMapFunction. The function takes a line (String) and
+	 * splits it into multiple pairs in the form of "(word,1)" (Tuple2<String,
+	 * Integer>).
 	 */
-	public static final class LineSplitter implements FlatMapFunction<String, Tuple3<String, String, String>> {
+	public static final class LineSplitter implements MapFunction<String, Tuple3<String, String, String>> {
 
 		@Override
-		public void flatMap(String value, Collector<Tuple3<String, String, String>> out) {
-			
+		public Tuple3<String, String, String> map(String value) throws Exception {
 			// normalize and split the line
 			String[] tokens = value.toLowerCase().split(" ");
 			tokens[0] = tokens[0].split("#")[1];
 			tokens[1] = tokens[1].split("#")[1];
-			String[] tab  = tokens[2].split("\\^\\^");
-			if (tab.length == 1){
+			String[] tab = tokens[2].split("\\^\\^");
+			if (tab.length == 1) {
 				tokens[2] = tab[0].split("#")[1];
-			}
-			else{
+			} else {
 				tokens[2] = tab[0];
 			}
-			
-			out.collect(new Tuple3<String, String,String>(tokens[0], tokens[1], tokens[2]));
-			
-			// emit the pairs
-//			for (String token : tokens) {
-//				if (token.length() > 0) {
-//					out.collect(new Tuple3<String, String,String>(token, 1));
-//				}
-//			}
-			
+			return new Tuple3<String, String, String>(tokens[0], tokens[1], tokens[2]);
 		}
-	}	
+	}
+
+	public static final class InputAnalyze
+			implements FlatMapFunction<Tuple3<String, String, String>, Tuple4<Integer, Integer, Float, String>> {
+
+		List<Integer> listSensors;
+	
+
+		public InputAnalyze(List<Integer> listSensors) {
+			this.listSensors = listSensors;
+
+		}
+
+		@Override
+		public void flatMap(Tuple3<String, String, String> value,
+				Collector<Tuple4<Integer, Integer, Float, String>> out) throws Exception {
+
+			switch (value.f1) {
+			
+			case "machine>":
+				StreamRabbitMQ.currentMachine = Integer.parseInt(value.f2.split("\\_|>")[1]);
+				break;
+
+			case "observationresulttime>":
+				StreamRabbitMQ.currentObsGroup = Integer.parseInt(value.f0.split("\\_|>")[1]);
+//				System.out.println("Observation group : " + StreamRabbitMQ.currentObsGroup);
+				break;
+
+			case "observedproperty>":
+
+				// System.out.println("Numéro de capteur : " +
+				// Integer.parseInt(value.f2.split("\\_|>")[1]));
+				// System.out.println("Numéro d'observation : " +
+				// Integer.parseInt(value.f0.split("\\_|>")[1]));
+
+				int sensorNb = Integer.parseInt(value.f2.split("\\_|>")[1]);
+				int obsNb = Integer.parseInt(value.f0.split("\\_|>")[1]);
+
+//				System.out.println(listSensors.contains(sensorNb));
+//				System.out.println(listSensors);
+//				System.out.println(sensorNb);
+
+				if (listSensors.contains(sensorNb)) {
+					StreamRabbitMQ.mapObsSensors.put(obsNb, sensorNb);
+//					System.out.println("Numéro d'observation enregisté");
+				} else {
+//					System.out.println("Ce capteur ne nous interesse pas !!");
+				}
+
+				break;
+
+			case "valueliteral>":
+
+				String[] tab = value.f0.split("\\_|>");
+
+				if (tab[0].split("\\_")[0].equals("timestamp")) {
+					StreamRabbitMQ.currentTimestamp = value.f2;
+//					System.out.println("Timestamp mis à jour");
+				}
+
+				else if (tab[0].equals("value") && StreamRabbitMQ.mapObsSensors.containsKey(Integer.parseInt(tab[1]))) {
+//					System.out.println(" ================= > " + "Capteur : "
+//							+ StreamRabbitMQ.mapObsSensors.get(Integer.parseInt(tab[1])) + " | Valeur : "
+//							+ Float.parseFloat(value.f2.split("\"")[1]));
+
+					out.collect(new Tuple4<Integer, Integer, Float, String>(
+							StreamRabbitMQ.currentMachine,
+							StreamRabbitMQ.mapObsSensors.get(Integer.parseInt(tab[1])) ,
+							Float.parseFloat(value.f2.split("\"")[1]) ,
+							StreamRabbitMQ.currentTimestamp));
+
+					StreamRabbitMQ.mapObsSensors.remove(Integer.parseInt(tab[1]));
+
+				}
+				break;
+
+			}
+		}
+	}
 }
