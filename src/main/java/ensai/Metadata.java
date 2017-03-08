d3106be4ac5f5fab3559ec1994b951ac1339d384
ak <andreaknrtzr@aol.com>
2017-03-08 15:58:44 +0100

    metadata pour les molding

--- src/main/java/ensai/Metadata.java
+++ src/main/java/ensai/Metadata.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple5;
 
 
@@ -13,8 +16,17 @@ public class Metadata {
 
 	static String[] lineSplit = new String[3];
 	//Tuple type de la machine, numero de la machine, capteurs à considérer, nbre de clusters pour ces capteurs, seuils pour ces capteurs
-	public static Tuple5<String, Integer, List<Integer>, List<Integer>, List<Float> > metaUneMachine = new Tuple5<String, Integer, List<Integer>, List<Integer>, List<Float> >(); 
+	//public static Tuple5<String, Integer, List<Integer>, List<Integer>, List<Float> > metaUneMachine = new Tuple5<String, Integer, List<Integer>, List<Integer>, List<Float> >(); 
 
+	//Hasmap avec en clef MoldingMachine_0, et en valeur les trois listes des capteurs, nbre clusters et seuils
+	//public static HashMap<String, Tuple3<List<Integer>, List<Integer>, List<Float>>> meta = new HashMap<String, Tuple3<List<Integer>, List<Integer>, List<Float>>>();
+
+	public static HashMap<Integer, Tuple2<Integer, Float>> moldingMeta = new HashMap<Integer, Tuple2<Integer, Float>>();
+	static String numMachineCapteur = "";
+	static String numCapteur = "";
+	static String nbClusters = ""; 
+	static String seuil  = "";
+		
 	public static void main(String[] args) throws IOException {
 
 
@@ -54,48 +66,55 @@ public class Metadata {
 
 			//System.out.println(lineSplit[0] + " " + lineSplit[1] + " " + lineSplit[2]);
 
+
+
+
+
 			//Tri
-			String MachineModel = "";
-			String numMachineCapteur = "";
 			switch (lineSplit[1]) {
-			
-			//le MachineModel suivi
-			case "hasModel>":
-				MachineModel = lineSplit[2].split(">")[0];
-				metaUneMachine.f0 = MachineModel ; 
- 				metaUneMachine.f1 = Integer.parseInt(lineSplit[0].split("_")[1].split(">")[0]);
-				break;
-				
+
 			//le numero de capteur
 			case "hasProperty>":
-				if(lineSplit[0].split(">")[0] == MachineModel){
-					numMachineCapteur = lineSplit[2].split(">")[0];
-					metaUneMachine.f2.add(Integer.parseInt(lineSplit[2].split("_")[2].split(">")[0]));
-				}
+				numMachineCapteur = lineSplit[2].split(">")[0];
+				numCapteur = numMachineCapteur.split("_")[2];
+				//}
 				break;
-			
-			//le nombre de clusters
+
+				//le nombre de clusters
 			case "hasNumberOfClusters>":
-				if(lineSplit[0].split(">")[0] == numMachineCapteur){
-					metaUneMachine.f3.add(Integer.parseInt(lineSplit[2].split("\"")[2].split("\"")[0]));
+				if(lineSplit[0].contains(numMachineCapteur)){
+					nbClusters = lineSplit[2].split("\"")[1];
 				}
 				break;
-			
-			//le seuil
+
+				//le seuil
 			case "valueLiteral>":
-				if(lineSplit[0].split(">")[0] == numMachineCapteur){
-					metaUneMachine.f4.add(Float.parseFloat(lineSplit[2].split("\"")[2].split("\"")[0]));
+				if(lineSplit[0].contains(numMachineCapteur)){
+					seuil = lineSplit[2].split("\"")[1];
+					break;
 				}
-				break;
-			
 			}
-			
-			System.out.println(metaUneMachine.f0 + " " + metaUneMachine.f1 + " " + metaUneMachine.f2 + " " + metaUneMachine.f3 + " " + metaUneMachine.f4);
-			
+
+			if(numMachineCapteur!="" && numCapteur!="" && nbClusters!="" && seuil!=""){
+				Tuple2<Integer, Float> clustersEtseuil = new Tuple2<Integer, Float>();
+				clustersEtseuil.f0 = Integer.parseInt(nbClusters);
+				clustersEtseuil.f1 = Float.parseFloat(seuil);
+				moldingMeta.put(Integer.parseInt(numCapteur), clustersEtseuil);
+			}
+
+			//			System.out.println(numMachineCapteur);
+			//			System.out.println(numCapteur);
+			//			System.out.println(nbClusters);
+			//			System.out.println(seuil);
+
 		}
+		System.out.println(moldingMeta);
+
 		br.close();
 
 
 	}
 
 }
+
+
