cf6fd60f2f5555eb5105f8c021f211751c8a66b0
ak <andreaknrtzr@aol.com>
2017-03-13 15:03:39 +0100

    nouveau Send

--- src/main/java/ensai/Send.java
+++ src/main/java/ensai/Send.java
@@ -6,16 +6,24 @@ import com.rabbitmq.client.Connection;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
 
 import com.rabbitmq.client.Channel;
 
 public class Send {
 	private final static String QUEUE_NAME = "coucou";
+	static List<byte[]> intermediaire = new ArrayList<byte[]>();
+	static List<String> intermediaire0 = new ArrayList<String>();
 
-	public static void main(String[] argv) throws java.io.IOException, TimeoutException {
+	public static void main(String[] argv) throws java.io.IOException, TimeoutException, InterruptedException {
 
-		int waitTime = 5;
+		int waitTime = 0;
 		int i = 1;
 		// create a connection to the server
 		ConnectionFactory factory = new ConnectionFactory();
@@ -23,28 +31,65 @@ public class Send {
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
-//		File f = new File("./ressources/molding_machine_10M.nt");
-		File f = new File("./ressources/molding_machine_10M.nt");
+		//		File f = new File("./ressources/molding_machine_10M.nt");
+		//		File f = new File("./ressources/molding_machine_10M.nt");
 
-		BufferedReader br = new BufferedReader(new FileReader(f));
-		String line;
+		String fileName = "./ressources/molding_machine_10M.nt";
+		try {
+			intermediaire0 = Files.readAllLines(Paths.get(fileName));
+			
+			//stream.forEach(System.out::println);
+			//intermediaire.add(stream.forEach(intermediaire.add(e)););
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		// declare a queue for us to send to; then we can publish a message to
 		// the queue
 		channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-		while ((line = br.readLine()) != null) {
-			
-			if (i%945 == 0){
-				try{
-					System.out.println("Waaaaaaaaaaaaait");
-					Thread.sleep(waitTime*1000);
-				}catch (InterruptedException e) {}
-			}
-			
-			channel.basicPublish("", QUEUE_NAME, null, line.getBytes());
-			System.out.println(" [x] Sent '" + line + "'");
-			i++;
+//		try{
+//			System.out.println("Waaaaaaaaaaaaait");
+//			Thread.sleep(waitTime*1000);
+//		}catch (InterruptedException e) {}
+		
+		for(int k = 0; k < 9440; k++){
+			intermediaire.add(intermediaire0.get(k).getBytes());
 		}
-		br.close();
+		
+		int c = 0;
+		long t1 = System.currentTimeMillis();
+		for(int h = 0; h < 9440; h++){
+			c++;
+			//Thread.sleep(waitTime*1000);
+			channel.basicPublish("", QUEUE_NAME, null, intermediaire.get(h)); // t1 = 110
+			//System.out.println(intermediaire0.size());
+			//System.out.println(" [x] Sent '" + intermediaire0.get(h) + "'");
+			if(c==9440){
+				System.out.println(System.currentTimeMillis()-t1);
+			}
+		}
+		
+		
+
+		//		BufferedReader br = new BufferedReader(new FileReader(f));
+		//		String line;
+		// declare a queue for us to send to; then we can publish a message to
+		// the queue
+		//		channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+		//		while ((line = br.readLine()) != null) {
+		//			
+		//			if (i%945 == 0){
+		//				try{
+		//					System.out.println("Waaaaaaaaaaaaait");
+		//					Thread.sleep(waitTime*1000);
+		//				}catch (InterruptedException e) {}
+		//			}
+		//			
+		//			channel.basicPublish("", QUEUE_NAME, null, line.getBytes());
+		//			System.out.println(" [x] Sent '" + line + "'");
+		//			i++;
+		//		}
+		//		br.close();
 
 		// String message = "Hello world !";
 
